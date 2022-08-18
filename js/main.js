a44e268ce9705257e2bc74f115a358a73af0ede7
Mirismoil <yuldashevmirismoil3@gmail.com>
2022-08-19 03:42:26 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// let btn_1 = document.querySelector(".hero-item__img");
+// let btn_2 = document.querySelector(".hero-item__img");
+// let btn_3 = document.querySelector(".hero-item__img");
+// let btn_4 = document.querySelector(".hero-item__img");
+// let btn_5 = document.querySelector(".hero-item__img");
+
+// btn_1.addEventListener("click", function(){
+//     this.hero-item__list
+// })
+
+// const button = document.getElementsByClassName(".hero-item__img")
+// const list = document.getElementsByClassName("hero-item__list")
+
+
+
+
+// let item__img = document.querySelector(".hero-item__img");
+
+// item__img.forEach(function(item__img){
+    //     item__img.addEventListener("click".)
+    // })
+    // document.querySelectorAll(".hero-item__img").forEach(e=> {
+        //     e.addEventListener('click', e =>{
+            //         const menu = e.currentTarget.dataset.path;
+            //         document.querySelectorAll('hero-item__list').forEach(e => {
+                //             if(!document.querySelector('[data-target-$(menu)]').classList.contains('open')){
+//                 document.querySelector('[data-target-$(menu)]').classList.add('menu-active');
+//             }
+//         })
+//     })
+// })
+
+// buttun .hero-item__img
+// ul list=hero-item__list
+document.querySelector('.hero-item__img').addEventListener('click', function(){
+    document.querySelector(".hero-item__list").classList.toggle('menu-active');
+})
+document.querySelector('.hero-item__img').addEventListener('click', function(){
+    document.querySelector(".hero-item__list2").classList.toggle('menu-active2');
+})
+document.querySelector('.hero-item__img').addEventListener('click', function(){
+    document.querySelector(".hero-item__list3").classList.toggle('menu-active3');
+})
+document.querySelector('.hero-item__img').addEventListener('click', function(){
+    document.querySelector(".hero-item__list4").classList.toggle('menu-active4');
+})
+document.querySelector('.hero-item__img').addEventListener('click', function(){
+    document.querySelector(".hero-item__list5").classList.toggle('menu-active5');
+})
+function timeShover(){
+    var timeNow = new Date();
+    
+    document.querySelector(".hours").textContent = timeNow.getHours();
+    document.querySelector(".minutes").textContent = timeNow.getMinutes();
+    document.querySelector(".seconds").textContent = timeNow.getSeconds();
+}
+timeShover();
+
+setInterval(function(){
+    timeShover
+},1000);
+
+timeShover();
+
+
